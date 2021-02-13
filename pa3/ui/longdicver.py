31d24c201367446c4124391607d28980fd4fdd2d
Jake Underland <jaked0626@cs.uchicago.edu>
2021-02-13 13:38:23 -0600

    one more time

--- pa3/ui/longdicver.py
+++ pa3/ui/longdicver.py
@@ -41,76 +41,6 @@ input_options = {"dept": {"SELECT": set(["dept", "course_num", "title"]),
                          "WHERE": "sections.enrollment <= ?"}}
 
 
-def select_func(args_from_ui):
-    outputs_to_fields = {"dept": "courses.", "course_num": "courses.", "title": "courses.", "section_num": "sections.", "day": "meeting_patterns.", "time_start": "meeting_patterns.", "time_end": "meeting_patterns.", "building": "b.building_code AS ", "walking_time": "time_between(a.lon, a.lat, b.lon, b.lat) AS ", "enrollment": "sections."}
-    ordered_outputs = ["dept", "course_num", "section_num", "day", "time_start", "time_end", "building", "walking_time", "enrollment", "title"]
-    query = set()
-    query_str = []
-    for input_, dic in input_options.items():
-        if input_ in args_from_ui.keys():
-            query.update(dic["SELECT"])
-    
-    if query:
-        for select_category in ordered_outputs:
-            if select_category in query:
-                query_str.append(select_category)
-        query_str = list(map(lambda x: "{}{}".format(outputs_to_fields[x], x), query_str))
-        query_str = "SELECT " + ", ".join(query_str)
-        
-    return query_str
-
-def from_func(args_from_ui):
-    'Creates the From and Join Command'
-    query = set()
-
-    for input_ in args_from_ui.keys():
-        query.update(input_options[input_]["FROM JOIN"])
-    if query:
-        query = "FROM " + " JOIN ".join(query)
-    else:
-        query = []
-
-    return query
-
-def on_func(args_from_ui):
-    'Creates the ON Command'
-    query = set()
-
-    for input_ in args_from_ui.keys():
-        query.update(input_options[input_]["ON"])
-    if query:
-        query = "ON " + " AND ".join(query)
-    else:
-        query = []
-
-    return query
-
-def where_func(args_from_ui): 
-
-    query = []
-    tupleq = tuple()
-    for input_, value in args_from_ui.items():
-        if isinstance(value, list):
-            subquery = []
-            for instance in value:
-                subquery.append(input_options[input_]["WHERE"])
-                tupleq += (instance,)
-            query += ["(" + " OR ".join(subquery) + ")"]
-        else: 
-            query.append(input_options[input_]["WHERE"])
-            tupleq += (value, )
-    if query:
-        query = "WHERE " +  " AND ".join(query)
-
-    return query, tupleq
-
-def groupby_func(args_copy):
-    num_terms = len(args_copy["terms"])
-    if num_terms > 1:
-        return "GROUP_BY courses.course_id HAVING COUNT(*) >= ?", (num_terms,)
-
-
-
 # Use this filename for the database
 DATA_DIR = os.path.dirname(__file__)
 DATABASE_FILENAME = os.path.join(DATA_DIR, 'course-info.db')
@@ -140,5 +70,88 @@ def find_courses(args_from_ui):
     args_copy = dict(args_from_ui)
     if args_copy.get("terms"):
         args_copy["terms"] = args_copy["terms"].split()
-    pass
-    #return '{} {} {} {}'.format(slct, frm, on, whr)
+    
+    query1 = select_func(args_copy)
+    query2 = from_func(args_copy)
+    query3 = on_func(args_copy)
+    query4, variables1 = where_func(args_copy)
+    query5, variables2 = groupby_func(args_copy)
+
+    return ( " ".join([query1, query2, query3, query4, query5], variables1 + variables2)
+
+
+def select_func(args_from_ui):
+    outputs_to_fields = {"dept": "courses.", "course_num": "courses.", "title": "courses.", "section_num": "sections.", "day": "meeting_patterns.", "time_start": "meeting_patterns.", "time_end": "meeting_patterns.", "building": "b.building_code AS ", "walking_time": "time_between(a.lon, a.lat, b.lon, b.lat) AS ", "enrollment": "sections."}
+    ordered_outputs = ["dept", "course_num", "section_num", "day", "time_start", "time_end", "building", "walking_time", "enrollment", "title"]
+    query = set()
+    query_str = []
+    for input_, dic in input_options.items():
+        if input_ in args_from_ui.keys():
+            query.update(dic["SELECT"])
+    
+    if query:
+        for select_category in ordered_outputs:
+            if select_category in query:
+                query_str.append(select_category)
+        query_str = list(map(lambda x: "{}{}".format(outputs_to_fields[x], x), query_str))
+        query_str = ["SELECT " + ", ".join(query_str)]
+        
+    return query_str
+
+
+def from_func(args_from_ui):
+    'Creates the From and Join Command'
+    query = set()
+
+    for input_ in args_from_ui.keys():
+        query.update(input_options[input_]["FROM JOIN"])
+    if query:
+        query = ["FROM " + " JOIN ".join(query)]
+    else:
+        query = []
+
+    return query
+
+def on_func(args_from_ui):
+    'Creates the ON Command'
+    query = set()
+
+    for input_ in args_from_ui.keys():
+        query.update(input_options[input_]["ON"])
+    if query:
+        [query = "ON " + " AND ".join(query)]
+    else:
+        query = []
+
+    return query
+
+def where_func(args_from_ui): 
+
+    args_copy = dict(args_from_ui)
+    if args_copy.get("terms"):
+        args_copy["terms"] = args_copy["terms"].split()
+
+    query = []
+    tupleq = tuple()
+    for input_, value in args_from_ui.items():
+        if isinstance(value, list):
+            subquery = []
+            for instance in value:
+                subquery.append(input_options[input_]["WHERE"])
+                tupleq += (instance,)
+            query += ["(" + " OR ".join(subquery) + ")"]
+        else: 
+            query.append(input_options[input_]["WHERE"])
+            tupleq += (value, )
+    if query:
+        [query = "WHERE " +  " AND ".join(query)]
+
+    return query, tupleq
+
+def groupby_func(args_copy):
+    num_terms = len(args_copy["terms"])
+    if num_terms > 1:
+        return ["GROUP_BY courses.course_id HAVING COUNT(*) >= ?"], (num_terms,)
+
+
+
