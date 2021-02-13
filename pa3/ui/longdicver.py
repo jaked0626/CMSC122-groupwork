42ed06dd855fa1d2ac226319433eac9c7b0ad34b
Jake Underland <jaked0626@cs.uchicago.edu>
2021-02-13 13:13:12 -0600

    one more time

--- pa3/ui/longdicver.py
+++ pa3/ui/longdicver.py
@@ -8,11 +8,9 @@ input_options = {"dept": {"SELECT": set(["dept", "course_num", "title"]),
                           "ON": set([]),
                           "WHERE": "courses.dept = ?"},
                  "terms": {"SELECT": set(["dept", "course_num", "title"]),
-                          "FROM JOIN": set(["courses"]), 
+                          "FROM JOIN": set(["courses", "catalog_index"]), 
                           "ON": set(["courses.course_id = catalog_index.course_id"]),
-                          "WHERE": "catalog_index.word = ?",
-                          "GROUP_BY": "courses.course_id",
-                          "HAVING": "COUNT(*) >= ?"},
+                          "WHERE": "catalog_index.word = ?"},
                  "day": {"SELECT": set(["dept", "course_num", "section_num", "day", "time_start", "time_end"]),
                          "FROM JOIN": set(["courses", "sections", "meeting_patterns"]), 
                          "ON": set(["courses.course_id = sections.course_id", "sections.meeting_pattern_id = meeting_patterns.meeting_pattern_id"]),
@@ -28,7 +26,7 @@ input_options = {"dept": {"SELECT": set(["dept", "course_num", "title"]),
                  "walking_time":  {"SELECT": set(["dept", "course_num", "section_num", "day", "time_start", "time_end", "building", "walking_time"]),
                          "FROM JOIN": set(["courses", "sections", "meeting_patterns", "gps AS a"]), 
                          "ON": set(["courses.course_id = sections.course_id", "sections.meeting_pattern_id = meeting_patterns.meeting_pattern_id", "sections.building_code = a.building_code"]),
-                         "WHERE": "walking_time =< ?"},
+                         "WHERE": "walking_time <= ?"},
                  "building": {"SELECT": set(["dept", "course_num", "section_num", "day", "time_start", "time_end", "building", "walking_time"]),
                          "FROM JOIN": set(["courses", "sections", "meeting_patterns", "gps AS b"]), 
                          "ON": set(["courses.course_id = sections.course_id", "sections.meeting_pattern_id = meeting_patterns.meeting_pattern_id", "sections.building_code = b.building_code"]),
@@ -42,11 +40,10 @@ input_options = {"dept": {"SELECT": set(["dept", "course_num", "title"]),
                          "ON": set(["courses.course_id = sections.course_id", "sections.meeting_pattern_id = meeting_patterns.meeting_pattern_id"]),
                          "WHERE": "sections.enrollment <= ?"}}
 
-outputs_to_fields = {"dept": "courses.", "course_num": "courses.", "title": "courses.", "section_num": "sections.", "day": "meeting_patterns.", "time_start": "meeting_patterns.", "time_end": "meeting_patterns.", "building": "b.building_code AS ", "walking_time": "time_between(a.lon, a.lat, b.lon, b.lat) AS ", "enrollment": "sections."}
-
-ordered_outputs = ["dept", "course_num", "section_num", "day", "time_start", "time_end", "building", "walking_time", "enrollment", "title"]
 
 def select_func(args_from_ui):
+    outputs_to_fields = {"dept": "courses.", "course_num": "courses.", "title": "courses.", "section_num": "sections.", "day": "meeting_patterns.", "time_start": "meeting_patterns.", "time_end": "meeting_patterns.", "building": "b.building_code AS ", "walking_time": "time_between(a.lon, a.lat, b.lon, b.lat) AS ", "enrollment": "sections."}
+    ordered_outputs = ["dept", "course_num", "section_num", "day", "time_start", "time_end", "building", "walking_time", "enrollment", "title"]
     query = set()
     query_str = []
     for input_, dic in input_options.items():
@@ -88,10 +85,7 @@ def on_func(args_from_ui):
 
     return query
 
-def where_func(args_from_ui):
-
-    if args_from_ui.get("terms"):
-        args_from_ui["terms"] = args_from_ui["terms"].split() 
+def where_func(args_from_ui): 
 
     query = []
     tupleq = tuple()
@@ -105,8 +99,46 @@ def where_func(args_from_ui):
         else: 
             query.append(input_options[input_]["WHERE"])
             tupleq += (value, )
-
-    query = "WHERE " +  " AND ".join(query)
+    if query:
+        query = "WHERE " +  " AND ".join(query)
 
     return query, tupleq
 
+def groupby_func(args_copy):
+    num_terms = len(args_copy["terms"])
+    if num_terms > 1:
+        return "GROUP_BY courses.course_id HAVING COUNT(*) >= ?", (num_terms,)
+
+
+
+# Use this filename for the database
+DATA_DIR = os.path.dirname(__file__)
+DATABASE_FILENAME = os.path.join(DATA_DIR, 'course-info.db')
+
+
+def find_courses(args_from_ui):
+    '''
+    Takes a dictionary containing search criteria and returns courses
+    that match the criteria.  The dictionary will contain some of the
+    following fields:
+
+      - dept a string
+      - day a list with variable number of elements
+           -> ["'MWF'", "'TR'", etc.]
+      - time_start an integer in the range 0-2359
+      - time_end an integer in the range 0-2359
+      - walking_time an integer
+      - enroll_lower an integer
+      - enroll_upper an integer
+      - building a string
+      - terms a string: "quantum plato"]
+
+    Returns a pair: list of attribute names in order and a list
+    containing query results.
+    '''
+
+    args_copy = dict(args_from_ui)
+    if args_copy.get("terms"):
+        args_copy["terms"] = args_copy["terms"].split()
+    pass
+    #return '{} {} {} {}'.format(slct, frm, on, whr)
